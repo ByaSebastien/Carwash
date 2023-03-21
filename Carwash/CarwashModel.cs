2897b6d4a3e6ca533b378f673ca658c519854f2d
technifutur <technifutur@DESKTOP-EHN422D>
2023-03-21 11:59:25 +0100

    petite modif

--- Carwash/CarwashModel.cs
+++ Carwash/CarwashModel.cs
@@ -14,7 +14,7 @@ namespace Carwash
         {
             Del = Preparer;
             Del += delegate (Voiture v) { Console.WriteLine($"je lave la voiture : {v.Plaque} "); };
-            Del += (Voiture v) => Console.WriteLine($"je sèche la voiture : {v.Plaque} ");
+            Del += (Voiture v) => { Console.WriteLine($"je sèche la voiture : {v.Plaque} "); };
             Del += (Voiture v) => Console.WriteLine($"je finalise la voiture : {v.Plaque} ");
         }
         private void Preparer(Voiture v)
